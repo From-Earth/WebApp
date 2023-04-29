065ece74433e2a666d81dbf96a94487b3d29bcc8
PHILIPPE KAWAY PONTES SALES <philippe.kpsales@senacsp.edu.br>
2023-04-28 21:04:11 -0300

    feat: cria nova funcionalidade

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter , Routes, Route, Outlet, Navigate} from "react-router-dom";
 import { Login } from "./components/Login/Login";
-import { Cadastro } from "./components/Cadastro/Cadastro"
+import { Cadastro } from "./components/Cadastro/Cadastro";
+import Home from "./pages/home/Home";
 import Header from "./components/Header/Header";
 import App from "./App";
 import path from "path";
-import Home from "./pages/home/Home";
 import NotFound from "./pages/notFound/NotFound";
 
 
@@ -22,7 +22,8 @@ export default function PageRoutes(){
            
             <Route element={<DefaultPages/>}>
                 <Route element={<NotFound/>} path="*"/>
-                <Route element={<Navigate to = "/cadastro" replace/>} path="/"/>
+                <Route element={<Navigate to = "/home" replace/>} path="/"/>
+                <Route element={<Home/>} path="/home"/>
                 <Route element={<Login/>} path="/login"/>
                 <Route element={<Cadastro/>} path="/cadastro"/>
             </Route>
