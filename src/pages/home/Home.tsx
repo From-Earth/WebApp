c2db470214d91bae19d1f88388c0879a3e318aca
PHILIPPE KAWAY PONTES SALES <philippe.kpsales@senacsp.edu.br>
2023-04-28 22:09:02 -0300

    feat: adiciona novas funcionalidades

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,10 +7,10 @@ export default function Home () {
     const navigate = useNavigate()
     return (
         <>
-        <section className="login">
+        <section className="home">
             <h2>From Earth</h2>
-          <div className="">
-          <img src={celular}  width="600px"/>
+          <div className="imagem">
+          <img src={celular}  width="300px"/>
           </div>
             
         </section>
