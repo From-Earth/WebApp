26e033ff2783b355f56840094736b86b61b541e8
matheus <matheus.malmeida7@gmail.com>
2023-04-28 22:44:25 -0300

    Finalizando Marco II com Select e Update de Usuário

--- src/components/User/UserEdit/UserEdit.tsx
+++ src/components/User/UserEdit/UserEdit.tsx
@@ -3,6 +3,7 @@ import "./UserEdit.css";
 
 import spinner from "../../../assets/images/spinner.gif";
 import { getUser } from "../../../services/Usuario";
+import { useParams } from "react-router-dom";
 
 export default function UserEdit() {
   const [nome, setNome] = useState("");
@@ -13,60 +14,77 @@ export default function UserEdit() {
   const [logradouro, setLogradouro] = useState("");
   const [numero, setNumero] = useState("");
   const [complemento, setComplemento] = useState("");
-  const [load, setLoad] = useState();
-  const [data, setData] = useState([])
+  const [load, setLoad] = useState(false);
+  const [data, setData] = useState<Usuario | null>(null);
+  let {id} = useParams();
 
+  useEffect(() => {
+    setLoad(true);
+    getUser(id)
+      .then((resp) => setData(resp))
+      .catch(() => alert("Usuario não encontrado")).finally(() => setLoad(false));
 
-  function editar(){}
+    
+  }, []);
+
+  function editar() {}
   return (
     <section className="edit">
-      <h2>Alterar Dados</h2>
+      {data && (
+        <>
+          <h2>Alterar Dados</h2>
 
-      <label>Nome</label>
-      <input
-        type="nome"
-        name="nome"
-        placeholder="Digite o seu nome"
-        autoComplete="off"
-        required
-        value={nome}
-        onChange={(e) => setNome(e.target.value)}
-      ></input>
-      <label>Email</label>
-      <input
-        type="email"
-        name="email"
-        placeholder="Digite o seu e-mail"
-        autoComplete="off"
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      ></input>
-      <label>Telefone</label>
-      <input
-        type="telefone"
-        name="telefone"
-        placeholder="Digite o seu telefone"
-        autoComplete="off"
-        required
-        value={telefone}
-        onChange={(e) => setTelefone(e.target.value)}
-      ></input>
-      <label>Senha</label>
-      <input
-        type="senha"
-        name="senha"
-        placeholder="Digite o seu senha"
-        autoComplete="off"
-        required
-        value={senha}
-        onChange={(e) => setSenha(e.target.value)}
-      ></input>
+          <label>Nome</label>
+          <input
+            type="nome"
+            name="nome"
+            placeholder="Digite o seu nome"
+            autoComplete="off"
+            required
+            value={data.nome}
+            defaultValue={nome}
+            onChange={(e) => setNome(e.target.value)}
+          ></input>
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu e-mail"
+            autoComplete="off"
+            required
+            value={data.email}
+            defaultValue={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <label>Telefone</label>
+          <input
+            type="telefone"
+            name="telefone"
+            placeholder="Digite o seu telefone"
+            autoComplete="off"
+            required
+            value={data.telefone}
+            defaultValue={telefone}
+            onChange={(e) => setTelefone(e.target.value)}
+          ></input>
+          <label>Senha</label>
+          <input
+            type="senha"
+            name="senha"
+            placeholder="Digite o seu senha"
+            autoComplete="off"
+            required
+            value={data.senha}
+            defaultValue={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          ></input>
 
-<button className='btn' type='submit' onClick={editar}>Editar</button>
-            {load && (
-                <img src={spinner}  width="30px"/>
-            )}
+          <button className="btn" type="submit" onClick={editar}>
+            Editar
+          </button>
+        </>
+      )}
+      {load && <img src={spinner} width="30px" />}
     </section>
   );
 }
--- src/services/Usuario.ts
+++ src/services/Usuario.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { UsuarioLogin } from "../model/UsuarioLogin";
 
 export const api = axios.create({
-  baseURL: "https://webapisenac.azurewebsites.net/usuario",
+  baseURL: "http://localhost:8080/usuario",
 });
 
 export function postLogin(data: UsuarioLogin) {
